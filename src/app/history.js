24d8805fd1e7df60a07654cdaeb49c11b5d1b348
Alexey Zhiltsov <azhil89@gmail.com>
2017-10-06 16:07:04 +0700

    one last attempt

--- src/app/history.js
+++ src/app/history.js
@@ -1,5 +1,7 @@
 import createBrowserHistory from 'history/createBrowserHistory';
 
-const history = createBrowserHistory();
+const history = createBrowserHistory({
+    basename: process.env.BASENAME
+});
 
 export default history;
