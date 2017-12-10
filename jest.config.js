63e6adf44787831f52a7014974309676e97778f3
Alexey Zhiltsov <azhil89@gmail.com>
2017-12-11 00:21:38 +0100

    add test for media queries mixin

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "moduleDirectories": [
+        "node_modules",
+        "src"
+    ],
+    collectCoverage: true
+};
---
+++ src/utils-css/media.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import media from './media';
+
+const DIV = styled.div`
+    ${media.mobileS`background-color: white;`}
+    ${media.mobileM`background-color: black;`}
+    ${media.mobileL`background-color: grey;`}
+    ${media.tablet`background-color: yellow;`}
+    ${media.computer`background-color: orange;`}
+    ${media.screenL`background-color: red;`}
+    ${media.screenW`background-color: blue;`}
+`;
+
+it("generates media queries", () => {
+    const div = renderer.create(<DIV />).toJSON();
+
+    expect(div).toHaveStyleRule(
+        'background-color', 'white', {
+            media: 'screen and (min-width:320px)'
+        }
+    );
+
+    expect(div).toHaveStyleRule(
+        'background-color', 'black', {
+            media: 'screen and (min-width:375px)'
+        }
+    );
+
+    expect(div).toHaveStyleRule(
+        'background-color', 'grey', {
+            media: 'screen and (min-width:425px)'
+        }
+    );
+
+    expect(div).toHaveStyleRule(
+        'background-color', 'yellow', {
+            media: 'screen and (min-width:768px)'
+        }
+    );
+
+    expect(div).toHaveStyleRule(
+        'background-color', 'orange', {
+            media: 'screen and (min-width:992px)'
+        }
+    );
+
+    expect(div).toHaveStyleRule(
+        'background-color', 'red', {
+            media: 'screen and (min-width:1200px)'
+        }
+    );
+
+    expect(div).toHaveStyleRule(
+        'background-color', 'blue', {
+            media: 'screen and (min-width:1920px)'
+        }
+    );
+});
