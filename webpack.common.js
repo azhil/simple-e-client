ca6ccbadc4a62b3144bb8090c6decac687d89182
Alexey Zhiltsov <azhil89@gmail.com>
2017-12-11 00:21:38 +0100

    add test for media queries mixin

--- webpack.common.js
+++ webpack.common.js
@@ -23,8 +23,8 @@ module.exports = {
 
     resolve: {
         modules: [
-            path.join(__dirname, 'src'),
-            path.join(__dirname, 'node_modules')
+            path.join(__dirname, 'node_modules'),
+            path.join(__dirname, 'src')
         ]
     },
 
